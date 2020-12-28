c740112afae306a2f58016d55374d5996c4308b0
geospiza <geospiza@protonmail.com>
2020-12-27 23:42:52 -0800

    Fix MonsterDropCreator script

--- bin/MonsterDropCreator.sh
+++ bin/MonsterDropCreator.sh
@@ -5,5 +5,4 @@ export MAVEN_OPTS="-Xmx600m"
 mvn exec:java -Dexec.mainClass="tools.MonsterDropCreator" \
     -Drecvops=recvops.properties \
     -Dsendops=sendops.properties \
-    -Dwzpath=wz/ \
-    false
+    -Dwzpath=wz/
