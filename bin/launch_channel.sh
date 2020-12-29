089bd221701461594ec4b533673d062ff313ef2b
geospiza <geospiza@protonmail.com>
2020-12-28 22:22:50 -0800

    Use mvn exec:exec for launching server

--- bin/launch_channel.sh
+++ bin/launch_channel.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
-export MAVEN_OPTS="-Xmx600m"
-mvn exec:java -Dexec.mainClass="handling.channel.ChannelServer" \
-    -Djava.util.logging.config.file=conf/logging.properties \
-    -Dwzpath=wz/ \
-    -Ddb.config=conf/db.properties \
-    -Dcom.sun.management.jmxremote.local.only=false \
-    -Dcom.sun.management.jmxremote.ssl=false \
-    -Dcom.sun.management.jmxremote.authenticate=false \
-    -Dcom.sun.management.jmxremote.port=1098 \
-    -Dcom.sun.management.jmxremote.rmi.port=1098 \
-    -Djava.rmi.server.hostname=world \
-    -Dchannel.config=conf/channel.properties
+mvn exec:exec -Dexec.executable="java" \
+    -Dexec.workingdir=$PWD \
+    -Dexec.args="-cp %classpath:target/*.jar \
+        -Xmx600m \
+        -Dcom.sun.management.jmxremote.local.only=false \
+        -Dcom.sun.management.jmxremote.ssl=false \
+        -Dcom.sun.management.jmxremote.authenticate=false \
+        -Dcom.sun.management.jmxremote.port=1098 \
+        -Dcom.sun.management.jmxremote.rmi.port=1098 \
+        -Djava.rmi.server.hostname=world \
+        -Dwzpath=wz/ \
+        -Djava.util.logging.config.file=conf/logging.properties \
+        -Ddb.config=conf/db.properties \
+        -Dchannel.config=conf/channel.properties \
+        handling.channel.ChannelServer"
--- bin/launch_world.sh
+++ bin/launch_world.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")"/..
-export MAVEN_OPTS="-Xmx600m"
-mvn exec:java -Dexec.mainClass="handling.world.WorldServer" \
-    -Dwzpath=wz/ \
-    -Djava.util.logging.config.file=conf/logging.properties \
-    -Ddb.config=conf/db.properties \
-    -Dcom.sun.management.jmxremote.local.only=false \
-    -Dcom.sun.management.jmxremote.ssl=false \
-    -Dcom.sun.management.jmxremote.authenticate=false \
-    -Dcom.sun.management.jmxremote.port=1098 \
-    -Dcom.sun.management.jmxremote.rmi.port=1098 \
-    -Dcom.sun.management.jmxremote.host=0.0.0.0 \
-    -Djava.rmi.server.hostname=0.0.0.0 \
-    -Dworld.config=conf/world.properties
+mvn exec:exec -Dexec.executable="java" \
+    -Dexec.workingdir=$PWD \
+    -Dexec.args="-cp %classpath:target/*.jar \
+        -Xmx600m \
+        -Dcom.sun.management.jmxremote.local.only=false \
+        -Dcom.sun.management.jmxremote.ssl=false \
+        -Dcom.sun.management.jmxremote.authenticate=false \
+        -Dcom.sun.management.jmxremote.port=1098 \
+        -Dcom.sun.management.jmxremote.rmi.port=1098 \
+        -Dcom.sun.management.jmxremote.host=0.0.0.0 \
+        -Djava.rmi.server.hostname=0.0.0.0 \
+        -Dwzpath=wz/ \
+        -Djava.util.logging.config.file=conf/logging.properties \
+        -Ddb.config=conf/db.properties \
+        -Dworld.config=conf/world.properties \
+        handling.world.WorldServer
+    "
