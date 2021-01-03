4104c5245f983987971d19b585498dd88f7b4dea
geospiza <geospiza@protonmail.com>
2021-01-02 18:56:47 -0800

    Remove custom subnet stuff that doesnt work

--- src/client/MapleClient.java
+++ src/client/MapleClient.java
@@ -497,45 +497,6 @@ public class MapleClient {
     return loginok;
   }
 
-  /**
-   * Gets the special server IP if the client matches a certain subnet.
-   *
-   * @param subnetInfo A <code>Properties</code> instance containing all the subnet info.
-   * @param clientIPAddress The IP address of the client as a dotted quad.
-   * @param channel The requested channel to match with the subnet.
-   * @return <code>0.0.0.0</code> if no subnet matched, or the IP if the subnet matched.
-   */
-  public static String getChannelServerIPFromSubnet(
-    String clientIPAddress,
-    int channel
-  ) {
-    long ipAddress = IPAddressTool.dottedQuadToLong(clientIPAddress);
-    Properties subnetInfo = LoginServer.getInstance().getSubnetInfo();
-
-    if (subnetInfo.contains("net.login.subnetcount")) {
-      int subnetCount = Integer.parseInt(
-        subnetInfo.getProperty("net.login.subnetcount")
-      );
-      for (int i = 0; i < subnetCount; i++) {
-        String[] connectionInfo = subnetInfo
-          .getProperty("net.login.subnet." + i)
-          .split(":");
-        long subnet = IPAddressTool.dottedQuadToLong(connectionInfo[0]);
-        long channelIP = IPAddressTool.dottedQuadToLong(connectionInfo[1]);
-        int channelNumber = Integer.parseInt(connectionInfo[2]);
-
-        if (
-          ((ipAddress & subnet) == (channelIP & subnet)) &&
-          (channel == channelNumber)
-        ) {
-          return connectionInfo[1];
-        }
-      }
-    }
-
-    return "0.0.0.0";
-  }
-
   private void unban() {
     int i;
     try {
